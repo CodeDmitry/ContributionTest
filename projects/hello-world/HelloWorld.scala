3c47394029764903b3cb5e48e268b4958e09e5f5
dmitry.makhnin <dmitry.makhnin@gmail.com>
2016-04-02 16:51:43 -0400

    Added hello world program for Scala.

---
+++ projects/hello-world/HelloWorld.scala
@@ -0,0 +1,15 @@
+
+/**
+ * Hello World program for the Scala programming language.
+ * Written by Dmitry Makhnin.
+ */
+object HelloWorld {
+
+    def main(args : Array[String]) : Unit = {
+
+        println("Hello, World!");
+
+    }
+
+}
+
