5364c072dc683aea067945eb7bde52a419bf74da
dmitry.makhnin <dmitry.makhnin@gmail.com>
2016-04-05 16:56:58 -0400

    Wrote a HelloWorld program in Java.

---
+++ projects/hello-world/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Hello World program for the Java programming language.
+ * Written by Dmitry Makhnin.
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
