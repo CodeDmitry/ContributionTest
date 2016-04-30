1bd6ed8420a0dde6b8c675a881ff7b9ff750dc19
dmitry.makhnin <dmitry.makhnin@gmail.com>
2016-04-29 23:21:06 -0400

    Wrote a Hello World program in C#

---
+++ projects/hello-world/HelloWorld.cs
@@ -0,0 +1,14 @@
+/**
+ * A Hello World program written in C#.
+ * Written by Dmitry Makhnin.
+ */
+using System.IO;
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
